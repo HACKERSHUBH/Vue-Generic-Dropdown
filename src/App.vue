4d4f88e7d875c490332b110b80babba0fc028688
Shubham Sharma <Shubham_Sharma12@dell.com>
2020-07-21 20:53:29 +0530

    Adding first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- {{selectedOption}} -->
+     <dropdown :options="dataOptions" v-model="selectedOption"/>
+  </div>
+</template>
+
+<script>
+import dropdown from './components/dropdown.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    dropdown
+  },
+  data(){
+    return{
+      selectedOption: '',
+       dataOptions: {
+        'Data1': 'Data1',
+        'Data2': 'Data2',
+        'Data3': 'Data3',
+        'Data4': 'Data4',
+        'Data5': 'Data5',
+        'Data6': 'Data6',
+        'Data7': 'Data7'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/dropdown.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <select
+      class="w-full form-control form-select"
+      v-model="selectedOption"
+      @input="event => { $emit('input', event.target.value) }"
+    >
+      <option v-for="(option, name) in options" :value="option" :key="name">{{ name }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dropdown",
+  props: {
+    value: null,
+    options: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isOpen: false,
+      selectedOption: null
+    };
+  },
+  mounted() {
+    this.selectedOption = this.value;
+  },
+
+  watch: {
+    value: function(newValue) {
+      this.selectedOption = newValue;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
